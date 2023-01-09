f4bfbbc514933b63a126790eeafb58b3f9661556
etil.sol <81570777+etil2jz@users.noreply.github.com>
2023-01-09 10:47:48 +0100

    Déplacement OK
    Il manque l'algo

--- main.c
+++ main.c
@@ -36,13 +36,13 @@ int askTypePartie() {
         printf("Sélectionnez votre type de partie\n(1) JcJ\n(2) IA\n\n> ");
         scanf("%i", &rep);
 
-    } while (rep < 1 || rep > 2);
+    } while (rep < 0 || rep > 1);
     return rep;
 }
 
-void nomJoueurs(int partieVsIA[], joueurs j[]) {
+void nomJoueurs(int partieJcJIA[], joueurs j[]) {
     clear();
-    if (partieVsIA[0] == 1) {
+    if (partieJcJIA[0] == 1) {
         printf("Nom de l'équipe ");
         couleur(93);
         printf("jaune : ");
@@ -50,11 +50,9 @@ void nomJoueurs(int partieVsIA[], joueurs j[]) {
         fgets(j[0].nom, sizeof(j[0].nom), stdin);
         j[0].nom[strcspn(j[0].nom, "\n")] = 0;
         couleur(33);
-
         printf("\nNom de l'équipe ");
         couleur(91);
         printf("rouge : ");
-        while ((getchar()) != '\n');
         fgets(j[1].nom, sizeof(j[1].nom), stdin);
         j[1].nom[strcspn(j[1].nom, "\n")] = 0;
         couleur(33);
@@ -76,13 +74,78 @@ void initPlateau(int plateau[][7]) {
             plateau[i][j] = 0;
         }
     }
+    // 0 équivaut à une case libre, 1 à une case remplie de jaune, 2 à une case remplie de rouge
 }
 
 void affichagePlateau(int plateau[][7]) {
-    
+    couleur(33);
+    printf("  1 2 3 4 5 6 7\n");
+    printf("  +-+-+-+-+-+-+\n");
+    for (int i = 0; i < 6; i++) {
+        printf("%c", 'A' + i);
+        for (int j = 0; j < 7; j++) {
+            if (plateau[i][j] == 1) {
+                couleur(33);
+                printf("|");
+                couleur(93);
+                printf("O");
+            } else if (plateau[i][j] == 2) {
+                couleur(33);
+                printf("|");
+                couleur(91);
+                printf("O");
+            } else {
+                couleur(33);
+                printf("| ");
+            }
+        }
+        couleur(33);
+        printf("|\n");
+        printf("  +-+-+-+-+-+-+\n");
+    }
 }
 
-void menu(int plateau[][7], joueurs j[], int enCoursDeJeu[], int partieVsIA[]) {
+void boucleJeu(int plateau[][7], joueurs j[], int enCoursDeJeu[], int partieJcJIA[]) {
+    int retourmenu = 0, win = 0, tourJoueur = 1, cordX, cordY;
+    do {
+        if (tourJoueur == 3) tourJoueur = 1;
+        while ((getchar()) != '\n');
+        clear();
+        couleur(33);
+        affichagePlateau(plateau);
+        printf("\n\nAu tour du joueur ");
+        if (tourJoueur == 1) {
+            couleur(93);
+            printf("%s", j[0].nom);
+        } else {
+            couleur(91);
+            printf("%s", j[1].nom);
+        }
+        couleur(33);
+        printf(" !");
+        do {
+            printf("\n\nSélectionnez votre colonne (1 2 3 4 5 6 7) : ");
+            scanf("%i", &cordX);
+            cordX--;
+        } while ((cordX < 0 || cordX > 6 ) || plateau[0][cordX] != 0);
+        cordY = 5;
+        while (cordY > -1) {
+            if (plateau[cordY][cordX] == 0) {
+                if (tourJoueur == 1) {
+                    plateau[cordY][cordX] = 1;
+                }
+                else {
+                    plateau[cordY][cordX] = 2;
+                }
+                break;
+            }
+            cordY--;
+        }
+        tourJoueur++;
+    } while (win == 0 && retourmenu == 0);
+}
+
+void menu(int plateau[][7], joueurs j[], int enCoursDeJeu[], int partieJcJIA[]) {
     int rep;
     do {
         couleur(94);
@@ -93,9 +156,10 @@ void menu(int plateau[][7], joueurs j[], int enCoursDeJeu[], int partieVsIA[]) {
         scanf("%i", &rep);
         switch (rep) {
             case 1:
-                partieVsIA[0] = askTypePartie();
-                nomJoueurs(partieVsIA, j);
+                partieJcJIA[0] = askTypePartie();
+                nomJoueurs(partieJcJIA, j);
                 enCoursDeJeu[0] = 1;
+                boucleJeu(plateau, j, enCoursDeJeu, partieJcJIA);
                 break;
             case 2:
                 break;
@@ -115,9 +179,9 @@ void menu(int plateau[][7], joueurs j[], int enCoursDeJeu[], int partieVsIA[]) {
 }
 
 int main() {
-    int plateau[6][7], enCoursDeJeu[1], partieVsIA[1];
+    int plateau[6][7], enCoursDeJeu[1], partieJcJIA[1];
     joueurs j[2]; // Déclaration des joueurs (2 max)
     initPlateau(plateau);
     clear();
-    menu(plateau, j, enCoursDeJeu, partieVsIA);
+    menu(plateau, j, enCoursDeJeu, partieJcJIA);
 }
