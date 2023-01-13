b2127aa0bb1fde1ffbdfee331f2b2d88dfb508ad
etil.sol <81570777+etil2jz@users.noreply.github.com>
2023-01-13 10:34:09 +0100

    Update main.c

--- main.c
+++ main.c
@@ -105,16 +105,74 @@ void affichagePlateau(int plateau[][7]) {
     }
 }
 
-void boucleJeu(int plateau[][7], joueurs j[], int enCoursDeJeu[], int partieJcJIA[]) {
-    int retourmenu = 0, win = 0, tourJoueur = 1, cordX, cordY;
+int verifVictoire(int plateau[][7], int joueur[]) {
+    // Vérification des lignes
+    for (int i = 0; i < 6; i++) {
+        for (int j = 0; j < 7 - 3; j++) {
+            if (plateau[i][j] == joueur[0] && plateau[i][j + 1] == joueur[0] && plateau[i][j + 2] == joueur[0] && plateau[i][j + 3] == joueur[0]) return joueur[0];
+        }
+    }
+
+    // Vérification des colonnes
+    for (int i = 0; i < 6 - 3; i++) {
+        for (int j = 0; j < 7; j++) {
+            if (plateau[i][j] == joueur[0] && plateau[i + 1][j] == joueur[0] && plateau[i + 2][j] == joueur[0] && plateau[i + 3][j] == joueur[0]) return joueur[0];
+        }
+    }
+
+    // Vérification des diagonales de gauche à droite
+    for (int i = 0; i < 6 - 3; i++) {
+        for (int j = 0; j < 7 - 3; j++) {
+            if (plateau[i][j] == joueur[0] && plateau[i + 1][j + 1] == joueur[0] && plateau[i + 2][j + 2] == joueur[0] && plateau[i + 3][j + 3] == joueur[0]) return joueur[0]; 
+        }
+    }
+
+    // Vérification des diagonales de droite à gauche
+    for (int i = 0; i < 6 - 3; i++) {
+        for (int j = 0; j < 7; j++) {
+            if (plateau[i][j] == joueur[0] && plateau[i + 1][j - 1] == joueur[0] && plateau[i + 2][j - 2] == joueur[0] && plateau[i + 3][j - 3] == joueur[0]) return joueur[0];
+        }
+    }
+
+    return 0; // Pas de victoire
+}
+
+void writeSauvegarde(int plateau[][7], joueurs j[], int enCoursDeJeu[], int partieJcJIA[], int tourJoueur[]) {
+    FILE * sauvegarde; // Erreur de segmentation - à corriger
+    sauvegarde = fopen("sauvegarde.esiee", "w");
+    for (int i = 0; i < 6; i++) {
+        for (int j = 0; i < 7; j++) {
+            fprintf(sauvegarde, "%i\n", plateau[i][j]);
+        }
+    }
+    fprintf(sauvegarde, "j[0].nom=%s\nj[1].nom=%s\nenCoursDeJeu[0]=%i\npartieJcJIA[0]=%i\ntourJoueur[0]=%i\n", j[0].nom, j[1].nom, enCoursDeJeu[0], partieJcJIA[0], tourJoueur[0]);
+    fclose(sauvegarde);
+}
+
+void readSauvegarde(int plateau[][7], joueurs j[], int enCoursDeJeu[], int partieJcJIA[], int tourJoueur[]) {
+    FILE * sauvegarde = NULL;
+    sauvegarde = fopen("sauvegarde.esiee", "r");
+    if (sauvegarde == NULL) printf("\nAucune sauvegarde détectée dans le dossier du jeu\nFermeture...");
+    for (int i = 0; i < 6; i++) {
+        for (int j = 0; i < 7; j++) {
+            fscanf(sauvegarde, "%i\n", &plateau[i][j]);
+        }
+    }
+    fscanf(sauvegarde, "j[0].nom=%s\nj[1].nom=%s\nenCoursDeJeu[0]=%i\npartieJcJIA[0]=%i\ntourJoueur[0]=%i\n", &j[0].nom, &j[1].nom, &enCoursDeJeu[0], &partieJcJIA[0], &tourJoueur[0]);
+    fclose(sauvegarde);
+}
+
+void boucleJeu(int plateau[][7], joueurs j[], int enCoursDeJeu[], int partieJcJIA[], int tourJoueur[]) {
+    int retourmenu = 0, win = 0, cordX, cordY;
+    tourJoueur[0] = 1;
     do {
-        if (tourJoueur == 3) tourJoueur = 1;
+        if (tourJoueur[0] == 3) tourJoueur[0] = 1;
         while ((getchar()) != '\n');
         clear();
         couleur(33);
         affichagePlateau(plateau);
         printf("\n\nAu tour du joueur ");
-        if (tourJoueur == 1) {
+        if (tourJoueur[0] == 1) {
             couleur(93);
             printf("%s", j[0].nom);
         } else {
@@ -124,14 +182,14 @@ void boucleJeu(int plateau[][7], joueurs j[], int enCoursDeJeu[], int partieJcJI
         couleur(33);
         printf(" !");
         do {
-            printf("\n\nSélectionnez votre colonne (1 2 3 4 5 6 7) : ");
+            printf("\n\nSélectionnez votre colonne (1 2 3 4 5 6 7)\n> ");
             scanf("%i", &cordX);
             cordX--;
         } while ((cordX < 0 || cordX > 6 ) || plateau[0][cordX] != 0);
         cordY = 5;
         while (cordY > -1) {
             if (plateau[cordY][cordX] == 0) {
-                if (tourJoueur == 1) {
+                if (tourJoueur[0] == 1) {
                     plateau[cordY][cordX] = 1;
                 }
                 else {
@@ -141,12 +199,37 @@ void boucleJeu(int plateau[][7], joueurs j[], int enCoursDeJeu[], int partieJcJI
             }
             cordY--;
         }
-        tourJoueur++;
-    } while (win == 0 && retourmenu == 0);
+        win = verifVictoire(plateau, tourJoueur);
+        if (win != 0) break;
+        tourJoueur[0]++;
+        clear();
+        affichagePlateau(plateau);
+        do {
+            printf("\n\nSouhaitez-vous afficher le menu ? (1 pour oui, 2 pour non)\n> ");
+            scanf("%i", &retourmenu);
+        } while (retourmenu < 1 || retourmenu > 2);
+        if (retourmenu == 1) {
+            clear();
+            couleur(94);
+            printf("=========================================\n               PUISSANCE 4\n=========================================\n\n");
+            couleur(33);
+            printf("1. Reprendre la partie\n2. Sauvegarder la partie\n3. Quitter le jeu\n\n> ");
+            scanf("%i", &retourmenu);
+            switch (retourmenu) {
+                case 1: break;
+                case 2: writeSauvegarde(plateau, j, enCoursDeJeu, partieJcJIA, tourJoueur);
+                        break;
+                case 3: exit(0);
+                        break;
+            }
+        }
+    } while (win == 0);
+    clear();
 }
 
 void menu(int plateau[][7], joueurs j[], int enCoursDeJeu[], int partieJcJIA[]) {
-    int rep;
+    int rep, tourJoueur[1];
+    enCoursDeJeu[0] = 0;
     do {
         couleur(94);
         printf("=========================================\n               PUISSANCE 4\n=========================================\n\n");
@@ -156,19 +239,18 @@ void menu(int plateau[][7], joueurs j[], int enCoursDeJeu[], int partieJcJIA[]) 
         scanf("%i", &rep);
         switch (rep) {
             case 1:
+                initPlateau(plateau);
                 partieJcJIA[0] = askTypePartie();
                 nomJoueurs(partieJcJIA, j);
                 enCoursDeJeu[0] = 1;
-                boucleJeu(plateau, j, enCoursDeJeu, partieJcJIA);
+                boucleJeu(plateau,  j, enCoursDeJeu, partieJcJIA, tourJoueur);
                 break;
             case 2:
+                readSauvegarde(plateau, j, enCoursDeJeu, partieJcJIA, tourJoueur);
+                boucleJeu(plateau, j, enCoursDeJeu, partieJcJIA, tourJoueur);
                 break;
             case 3:
                 exit(0);
-                break;
-            case 4:
-                break;
-            case 5:
                 break;
             default:
                 clear();
@@ -181,7 +263,6 @@ void menu(int plateau[][7], joueurs j[], int enCoursDeJeu[], int partieJcJIA[]) 
 int main() {
     int plateau[6][7], enCoursDeJeu[1], partieJcJIA[1];
     joueurs j[2]; // Déclaration des joueurs (2 max)
-    initPlateau(plateau);
     clear();
     menu(plateau, j, enCoursDeJeu, partieJcJIA);
 }
